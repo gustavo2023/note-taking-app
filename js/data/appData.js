372720efa499955e1b0ff3bb997013c5d8d344a7
gustavo2023 <gustavo2023@gmail.com>
2025-06-06 17:38:55 -0400

    feat: implement addNotebook and deleteNotebook functions for notebook management

--- js/data/appData.js
+++ js/data/appData.js
@@ -9,3 +9,19 @@ export const initializeAppData = () => {
 export const getNotebooks = () => {
   return notebooks;
 };
+
+export const addNotebook = (name) => {
+  let newNotebook = {
+    id: Date.now().toString(),
+    name: name,
+    notes: [],
+  };
+
+  notebooks.push(newNotebook);
+  saveData(notebooks);
+};
+
+export const deleteNotebook = (notebookId) => {
+  notebooks = notebooks.filter((notebook) => notebook.id !== notebookId);
+  saveData(notebooks);
+};
